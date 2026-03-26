ab38b8f15905d53de7c00ab1a0bb26f27f3b0a56
rde <rde@companieshouse.gov.uk>
2026-03-26 11:15:19 +0000

    few formatting changes

--- src/main/resources/db/create_dpa.sql
+++ src/main/resources/db/create_dpa.sql
@@ -1,11 +1,9 @@
-CREATE SCHEMA IF NOT EXISTS addressmig_green;
-CREATE TABLE IF NOT EXISTS addressmig_green.dpa
-(
-    uprn INTEGER NOT NULL,
+CREATE SCHEMA IF NOT EXISTS lambda_demo;
+CREATE TABLE IF NOT EXISTS lambda_demo.dpa
+(uprn VARCHAR NOT NULL,
     organisation_name VARCHAR(60) ,
     department_name VARCHAR(60) ,
     sub_building_name VARCHAR(30) ,
-    building_number INTEGER,
     dependent_thoroughfare VARCHAR(80) ,
     thoroughfare VARCHAR(80) ,
     double_dependent_locality VARCHAR(35) ,
